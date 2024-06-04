fb16c0f854f29afad3197e1e3f2a0c3a51287a0c
Sebastian Machaj <team@blackredstudio.com>
2024-06-04 14:02:45 +0200

    5_1 - instalacja oraz konfiguracja biblioteki do MDX

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,48 @@
+import nextMDX from '@next/mdx';
+
+const withMDX = nextMDX({
+	extension: /\.mdx$/,
+	options: {
+		remarkPlugins: [],
+		rehypePlugins: [],
+	},
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.pexels.com',
-                pathname: '/photos/**'
-            }
-        ]
-    },
-    async redirects() {
-        return [
-            {
-                source: '/filmy-i-seriale',
-                destination: '/szukaj',
-                permanent: false
-            },
-            {
-                source: '/filmy-i-seriale/:type',
-                destination: '/szukaj',
-                permanent: false
-            },
-            // przekierowanie całej ścieżki
-            // {
-            //     source: '/szukaj/:slug*',
-            //     destination: '/',
-            //     permanent: false
-            // }
-        ]
-    }
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.pexels.com',
+				pathname: '/photos/**',
+			},
+		],
+	},
+	pageExtensions: ['mdx', 'ts', 'tsx'],
+	experimental: {
+		mdxRs: true,
+	},
+	async redirects() {
+		return [
+			{
+				source: '/filmy-i-seriale',
+				destination: '/szukaj',
+				permanent: false,
+			},
+			{
+				source: '/filmy-i-seriale/:type',
+				destination: '/szukaj',
+				permanent: false,
+			},
+			// przekierowanie całej ścieżki
+			// {
+			//     source: '/szukaj/:slug*',
+			//     destination: '/',
+			//     permanent: false
+			// }
+		];
+	},
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
