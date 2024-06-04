4b42cbba166d8649ad24cda7e28efb97d5f3c2e0
Sebastian Machaj <team@blackredstudio.com>
2024-06-04 18:41:38 +0200

    5_2 - podstawy mdx

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -10,8 +10,8 @@ export const metadata = {
 
 export default function BlogLayout({children}: TProps) {
     return (
-        <>
+        <article className="prose prose-invert xl:prose-xl">
             {children}
-        </>
+        </article>
     )
 }
