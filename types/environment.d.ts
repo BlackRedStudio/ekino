e1a14340c9141eb640732c7a033bbb4b4ea679be
Sebastian Machaj <team@blackredstudio.com>
2024-06-17 23:45:09 +0200

    8_1 + 8_2 - inicjalizacja bazy danych na vercel + biblioteka drizzle

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -2,6 +2,14 @@ export declare global {
     namespace NodeJS {
         interface ProcessEnv {
             MOVIE_DB_TOKEN: string;
+            POSTGRES_URL: string;
+            POSTGRES_PRISMA_URL: string;
+            POSTGRES_URL_NO_SSL: string;
+            POSTGRES_URL_NON_POOLING: string;
+            POSTGRES_USER: string;
+            POSTGRES_HOST: string;
+            POSTGRES_PASSWORD: string;
+            POSTGRES_DATABASE: string;
         }
     }
 }
