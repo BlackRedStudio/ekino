67b74abe8ed141fb70f64fda0072a3763906a952
Sebastian Machaj <team@blackredstudio.com>
2024-06-21 16:43:11 +0200

    9_2 - Biblioteka Zod - tworzenie pierwszych walidatorow

--- app/rejestracja/page.tsx
+++ app/rejestracja/page.tsx
@@ -1,12 +1,24 @@
-export const metadata = {
-    title: 'Rejestracja'
-}
+'use client'
+
+import FormField from "@/components/form-fields"
+import SubmitButton from "@/components/ui/submit-button"
+import { test } from "@/server/actions/user-actions"
+
 
 export default function RegisterPage() {
     return (
         <article>
             <section>
-                Rejestracja
+                <form action={test}>
+                    <FormField
+                       label="Test"
+                       name="test"
+                       type="text"
+                       placeholder="test akcji serverowej"
+                       required
+                   />
+                   <SubmitButton text="Wyślij" />
+                </form>
             </section>
         </article>
     )
